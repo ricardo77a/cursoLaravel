67faa0daf5b36e9def8a1749061c3914840e152b
Ricardo Arredondo Rïos <ricardo_77a@hotmail.com>
2019-08-19 17:19:40 -0500

    Datatables Español
    Solo se agrega un archivo Json que va en public/js y en main agregar llamada de archivo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,9 +11,17 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 		{{-- CSS de DataTables --}}
+		{{--
 		<link rel="stylesheet" type="text/css" href="{{ asset('css/jquery.dataTables.min.css') }}">
+		--}}
 		{{-- Jquery del Datatables --}}
+		{{--
 		<script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
+		 --}}
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap.css"/>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap.js"></script>
+
 	</head>
 	<body>
 
@@ -25,7 +33,11 @@
 		{{-- Script para llamar a DataTables --}}
 		<script type="text/javascript">
 			$(document).ready( function () {
-			    $('.dataTable').DataTable();
+			    $('.dataTable').DataTable({
+		            "language": {
+		                "url": "{{ asset('js/Spanish.json') }}"
+		            }
+			    });
 			});
 		</script>
 	</body>
